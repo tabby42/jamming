2c77bd4aa5339a1e8760ab87f51a6154fa933cdf
tabby42 <irene.theiss@gmx.at>
2018-04-19 14:45:06 +0200

    add constructor and hardcoded tracks, pass tracks to SearchResults

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,23 +4,46 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import PlayList from '../PlayList/PlayList';
 
+const trackResults = [
+  {
+    id: 1,
+    name: 'Song1',
+    artist: 'Artist1',
+    album: 'Album1'
+  },
+  {
+    id: 2,
+    name: 'Song2',
+    artist: 'Artist2',
+    album: 'Album2'
+  },
+  {
+    id: 3,
+    name: 'Song3',
+    artist: 'Artist3',
+    album: 'Album3'
+  }
+];
+
 class App extends Component {
   constructor (props) {
     super(props);
+    this.state = { searchResults: trackResults };
+    
   }
 
   render() {
     return (
       <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <PlayList />
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <PlayList />
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 }
