a7eb42e37f18b129726e523828db6c1dc54ef22f
tabby42 <irene.theiss@gmx.at>
2018-04-21 12:13:51 +0200

    add event handlers for onSearch and onClick

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+	      term: ''
+	    };
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this); 
+	}
+
+	handleTermChange(event) {
+   		this.setState({term: event.target.value});
+	}
+
+	handleSearch() {
+		this.props.onSearch(this.state.term);
+	}
+
 	render() {
 		return (
 			<div className="SearchBar">
-	          <input placeholder="Enter A Song Title" />
-	          <a>SEARCH</a>
+	          <input placeholder="Enter A Song Title" type="text" onChange={this.handleTermChange} />
+	          <a onClick={this.handleSearch} >SEARCH</a>
 	        </div>
 		);
 	}
