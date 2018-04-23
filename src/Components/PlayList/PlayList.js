061d73f979dce6e830a271cd9e3fc14de9d39894
tabby42 <irene.theiss@gmx.at>
2018-04-23 20:55:05 +0200

    add handleKeyPress and handleClick to reset input field on enter bzw save

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -6,19 +6,42 @@ import TrackList from '../TrackList/TrackList';
 class PlayList extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+	      inputValue: ''
+	    };
 		this.handleNameChange = this.handleNameChange.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
+		this.handleClick = this.handleClick.bind(this);
 	}
 
 	handleNameChange(e) {
 		this.props.onUpdatePlaylistName(e.target.value);
+		this.setState({
+			inputValue: e.target.value
+		});
+	}
+
+	//clear input on enter
+	handleKeyPress(e) {
+		if(e.key === 'Enter'){
+			e.target.value = '';
+	    }
+	}
+
+	handleClick(e) {
+		this.props.onSave();
+		this.setState({
+			inputValue: ''
+		});
 	}
 
 	render() {
 		return (
 			<div className="Playlist">
-				<input value={this.props.playlistName} onChange={this.handleNameChange}/>
+				<input placeholder="Enter Playlist Name" onChange={this.handleNameChange} onKeyPress={this.handleKeyPress} value={this.state.inputValue}/>
+				<h3>{this.props.playlistName}</h3>
 				<TrackList tracks={this.props.tracks} onRemove={this.props.onRemove} isRemoval={true} />
-				<a className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</a>
+				<a className="Playlist-save" onClick={this.handleClick} >SAVE TO SPOTIFY</a>
 			</div>
 		);
 	}
