69cc4df7f0da7038c3caaf1a8addb93ddfc67825
tabby42 <irene.theiss@gmx.at>
2018-04-24 00:02:19 +0200

    change heading

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -7,7 +7,7 @@ class SearchResults extends Component {
 	render() {
 		return (
 			<div className="SearchResults">
-			  <h2>Results</h2>
+			  <h3>Search Results</h3>
 			  	<TrackList tracks={this.props.tracks} onAdd={this.props.onAdd} isRemoval={false} />
 			</div>
 		);
