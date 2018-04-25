4216ac1d0cfd765a95c16e6e4cd7dd3331b17504
tabby42 <irene.theiss@gmx.at>
2018-04-25 13:08:03 +0200

    add message when no results where found

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -4,10 +4,17 @@ import './SearchResults.css';
 import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends Component {
+	renderNoResults() {
+		if (this.props.tracks.length === 0) {
+			return <p></p>;
+		}
+	}
+
 	render() {
 		return (
 			<div className="SearchResults">
 			  <h3>Search Results</h3>
+				<p>{this.props.tracks.length === 0 ? 'No Results. Try the Search Bar!' : ''}</p>
 			  	<TrackList tracks={this.props.tracks} onAdd={this.props.onAdd} isRemoval={false} />
 			</div>
 		);
