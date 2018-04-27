afa17033650ec2665810096f0adf4e1300152771
tabby42 <irene.theiss@gmx.at>
2018-04-27 16:45:12 +0200

    check if tracks are defined before rendering

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -7,14 +7,14 @@ class TrackList extends Component {
 	render() {
 		return(
 			<div className="TrackList">
-				{this.props.tracks.map( track => {
+				{this.props.tracks!== undefined ? this.props.tracks.map( track => {
 					return <Track track={track} 
 									key={track.id} 
 									onAdd={this.props.onAdd} 
 									onRemove={this.props.onRemove} 
 									isRemoval={this.props.isRemoval}
 							/>
-				})}
+				}) : ''}
 			</div>
 		);
 	}
