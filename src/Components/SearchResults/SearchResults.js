fb9dbb08e121b169d2196e2df2ed195ed31187a4
tabby42 <irene.theiss@gmx.at>
2018-04-27 15:45:20 +0200

    modify chekc fo rempty results

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -4,17 +4,11 @@ import './SearchResults.css';
 import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends Component {
-	renderNoResults() {
-		if (this.props.tracks.length === 0) {
-			return <p></p>;
-		}
-	}
-
 	render() {
 		return (
 			<div className="SearchResults">
 			  <h3>Search Results</h3>
-				<p>{this.props.tracks.length === 0 ? 'No Results. Try the Search Bar!' : ''}</p>
+				<p>{this.props.tracks === undefined ? 'No Results. Try the Search Bar!' : ''}</p>
 			  	<TrackList tracks={this.props.tracks} onAdd={this.props.onAdd} isRemoval={false} />
 			</div>
 		);
